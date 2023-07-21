8b11f2eb3d6a4b821c9a45bf26e111d480baad3b
julius caezar <julius.caezar25@gmail.com>
2023-07-21 17:39:02 -0400

    feat: commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+ 
+  return (
+    <div className="min-h-screen p-8 text-zinc-900 bg-gradient-to-r from-gray-200 via-gray-400 to-gray-600">
+      <div className="bg-white w-[1100px] mx-auto rounded-xl min-h-[720px] shadow-sm border-black/20 overflow-hidden grid grid-cols-[16rem_1fr]">
+        <aside className="bg-red-700 border-r border-r-zinc-100 p-4"></aside>
+        <main className="p-4"></main>
+      </div>
+    </div>
+  )
+}
+
+export default App;
