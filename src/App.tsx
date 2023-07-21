698238b45248aedeb63191ea46d7cc063abac4ac
julius caezar <julius.caezar25@gmail.com>
2023-07-21 17:58:23 -0400

    feat: criando os ícones do mac de abir, minimizar...

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,13 @@ function App() {
   return (
     <div className="min-h-screen p-8 text-zinc-900 bg-gradient-to-r from-gray-200 via-gray-400 to-gray-600">
       <div className="bg-white w-[1100px] mx-auto rounded-xl min-h-[720px] shadow-sm border-black/20 overflow-hidden grid grid-cols-[16rem_1fr]">
-        <aside className="bg-red-700 border-r border-r-zinc-100 p-4"></aside>
+        <aside className="bg-red-700 border-r border-r-zinc-100 p-4">
+          <div className="flex gap-2 group">
+            <button className="w-3 h-3 rounded-full bg-red-400"/>
+            <button className="w-3 h-3 rounded-full bg-yellow-400"/>
+            <button className="w-3 h-3 rounded-full bg-green-400"/>
+          </div>
+        </aside>
         <main className="p-4"></main>
       </div>
     </div>
