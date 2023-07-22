0d5c7e409b141c5eb1a1348148065cd065232183
julius caezar <julius.caezar25@gmail.com>
2023-07-22 13:06:49 -0400

    feat: importando o componente bubblebutton e os icones do radix/ criando os components bubblebutton e setando estilização

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import Editor from "./components/Editor";
 function App() {
  
   return (
-    <div className="min-h-screen p-8 text-zinc-900 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
-      <div className="bg-white w-[1100px] mx-auto rounded-xl min-h-[720px] shadow-sm border-black/20 overflow-hidden grid grid-cols-[16rem_1fr]">
-        <aside className="bg-zinc-100 border-r border-r-zinc-100 p-4">
+    <div className="min-h-screen p-8 text-zinc-50 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
+      <div className="bg-zinc-800 w-[1100px] mx-auto rounded-xl min-h-[720px] shadow-sm border-black/20 overflow-hidden grid grid-cols-[16rem_1fr]">
+        <aside className="bg-zinc-900 border-r border-r-zinc-700 p-4">
           <div className="flex gap-2 group">
             <button className="w-3 h-3 rounded-full bg-red-400"/>
             <button className="w-3 h-3 rounded-full bg-yellow-400"/>
