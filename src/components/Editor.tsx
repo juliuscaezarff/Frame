01fdd5d30328daefcba9d16dbf4d97f693c2774d
julius caezar <julius.caezar25@gmail.com>
2023-07-22 13:08:49 -0400

    importando os icones do radix e importando o component bubblebutton

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -3,8 +3,17 @@ import StarterKit from '@tiptap/starter-kit';
 import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight'
 import { lowlight } from 'lowlight';
 import js from 'highlight.js/lib/languages/javascript';
+import {
+  RxFontBold,
+  RxFontItalic,
+  RxStrikethrough,
+  RxCode,
+  RxChevronDown,
+  RxChatBubble
+} from 'react-icons/rx'
 
 import 'highlight.js/styles/atom-one-dark.css'
+import { BubbleButton } from './BubblleButton';
 
 lowlight.registerLanguage('js', js)
 
@@ -35,18 +44,31 @@ export default function Editor() {
           className='bg-zinc-700 shadow-xl border border-zinc-600 shadow-black/20 rounded-lg overflow-hidden flex divide-x divide-zinc-600' 
           editor={editor}
         >
-          <button className='p-2 text-zinc-200 text-sm flex items-center gap-1.5 font-medium leading-none hover:text-zinc-50 hover:bg-zinc-600 '>
-            bold
-          </button>
-          <button className='p-2 text-zinc-200 text-sm flex items-center gap-1.5 font-medium leading-none hover:text-zinc-50 hover:bg-zinc-600'>
-            bold
-          </button>
-          <button className='p-2 text-zinc-200 text-sm flex items-center gap-1.5 font-medium leading-none hover:text-zinc-50 hover:bg-zinc-600'>
-            bold
-          </button>
-          <button className='p-2 text-zinc-200 text-sm flex items-center gap-1.5 font-medium leading-none hover:text-zinc-50 hover:bg-zinc-600'>
-            bold
-          </button>
+          <BubbleButton>
+            Text
+            <RxChevronDown className='w-4 h-4'/>
+          </BubbleButton>
+          <BubbleButton>
+            Comment
+            <RxChatBubble className='w-4 h-4'/>
+          </BubbleButton>
+         <div className='flex items-center'>
+          <BubbleButton
+            onClick={() => editor.chain().focus().toggleBold().run()}
+            data-active={editor.isActive('bold')}
+          >
+              <RxFontBold className='w-4 h-4'/>
+            </BubbleButton>
+            <BubbleButton>
+              <RxFontItalic className='w-4 h-4'/>
+            </BubbleButton>
+            <BubbleButton>
+              <RxStrikethrough className='w-4 h-4'/>
+            </BubbleButton>
+            <BubbleButton>
+              <RxCode className='w-4 h-4'/>
+          </BubbleButton>
+         </div>
         </BubbleMenu>
       )}
     </>
