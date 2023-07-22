98533ba53efb47e5f2fe5d43dd2b2be2eef57023
julius caezar <julius.caezar25@gmail.com>
2023-07-22 01:05:36 -0400

    feat: adicionando novo gradient

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Editor from "./components/Editor";
 function App() {
  
   return (
-    <div className="min-h-screen p-8 text-zinc-900 bg-gradient-to-r from-gray-200 via-gray-400 to-gray-600">
+    <div className="min-h-screen p-8 text-zinc-900 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
       <div className="bg-white w-[1100px] mx-auto rounded-xl min-h-[720px] shadow-sm border-black/20 overflow-hidden grid grid-cols-[16rem_1fr]">
         <aside className="bg-zinc-100 border-r border-r-zinc-100 p-4">
           <div className="flex gap-2 group">
