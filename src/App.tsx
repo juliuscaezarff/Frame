26f222b8a9b890c70e4611e9b2533e47e6ad80e4
julius caezar <julius.caezar25@gmail.com>
2023-07-23 22:31:35 -0400

    refactor: indentando o código

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import Editor from "./components/Editor";
+import Editor from './components/Editor'
 
 function App() {
- 
   return (
     <div className="min-h-screen p-8 text-zinc-50 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
       <div className="bg-zinc-800 w-[1100px] mx-auto rounded-xl min-h-[720px] shadow-sm border-black/20 overflow-hidden grid grid-cols-[16rem_1fr]">
         <aside className="bg-zinc-900 border-r border-r-zinc-700 p-4">
           <div className="flex gap-2 group">
-            <button className="w-3 h-3 rounded-full bg-red-400"/>
-            <button className="w-3 h-3 rounded-full bg-yellow-400"/>
-            <button className="w-3 h-3 rounded-full bg-green-400"/>
+            <button className="w-3 h-3 rounded-full bg-red-400" />
+            <button className="w-3 h-3 rounded-full bg-yellow-400" />
+            <button className="w-3 h-3 rounded-full bg-green-400" />
           </div>
         </aside>
         <main className="p-4">
@@ -20,4 +19,4 @@ function App() {
   )
 }
 
-export default App;
+export default App
