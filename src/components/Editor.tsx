6e9e1b380f5d2c9824febbe71ddf092b7fe40f3f
julius caezar <julius.caezar25@gmail.com>
2023-07-24 21:27:10 -0400

    feat: Criando o FloatingMenu e adicionando buttons que criam ações de criar um h1 e etc

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -30,7 +30,7 @@ export default function Editor() {
         lowlight
       })
     ],
-    content: '<p>Hello World!</p>',
+    content: '<h1>Escreva algo</h1>',
     editorProps: {
       attributes: {
         class: 'outline-none'
@@ -47,6 +47,7 @@ export default function Editor() {
       {editor && (
         <FloatingMenu
           editor={editor}
+          className="bg-zinc-700 py-2 px-1 shadow-xl gap-1 border border-zinc-600 shadow-black/20 rounded-lg overflow-hidden flex flex-col"
           shouldShow={({ state }) => {
             const { $from } = state.selection
 
@@ -55,7 +56,40 @@ export default function Editor() {
             return currentLineText === '/'
           }}
         >
-          bold
+          <button
+            className="flex items-center gap-2 p-1 rounded min-w-[280px] hover:bg-zinc-600"
+          >
+            <img
+              src="http://www.notion.so/images/blocks/text/en-US.png"
+              alt="text"
+              className="w-12 border border-zinc-600 rounded"
+            />
+            <div className="flex flex-col text-left">
+              <span className="text-sm">Text</span>
+              <span className="text-xs text-zinc-400">
+                Just start writing with plain text.
+              </span>
+            </div>
+          </button>
+
+          <button
+            className="flex items-center gap-2 p-1 rounded min-w-[280px] hover:bg-zinc-600"
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 1 }).run()
+            }
+          >
+            <img
+              src="http://www.notion.so/images/blocks/header.57a7576a.png"
+              alt="text"
+              className="w-12 border border-zinc-600 rounded"
+            />
+            <div className="flex flex-col text-left">
+              <span className="text-sm">Heading 1</span>
+              <span className="text-xs text-zinc-400">
+                Big section heading.
+              </span>
+            </div>
+          </button>
         </FloatingMenu>
       )}
 
@@ -79,7 +113,10 @@ export default function Editor() {
             >
               <RxFontBold className="w-4 h-4" />
             </BubbleButton>
-            <BubbleButton>
+            <BubbleButton
+              onClick={() => editor.chain().focus().toggleItalic().run()}
+              data-active={editor.isActive('italic')}
+            >
               <RxFontItalic className="w-4 h-4" />
             </BubbleButton>
             <BubbleButton>
