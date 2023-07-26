642182a492cab4bdd4c3e5cd73780de4c4404b5d
julius caezar <julius.caezar25@gmail.com>
2023-07-26 16:51:59 -0400

    feat: Criando o button de heading 1

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -58,22 +58,6 @@ export default function Editor() {
         >
           <button
             className="flex items-center gap-2 p-1 rounded min-w-[280px] hover:bg-zinc-600"
-          >
-            <img
-              src="http://www.notion.so/images/blocks/text/en-US.png"
-              alt="text"
-              className="w-12 border border-zinc-600 rounded"
-            />
-            <div className="flex flex-col text-left">
-              <span className="text-sm">Text</span>
-              <span className="text-xs text-zinc-400">
-                Just start writing with plain text.
-              </span>
-            </div>
-          </button>
-
-          <button
-            className="flex items-center gap-2 p-1 rounded min-w-[280px] hover:bg-zinc-600"
             onClick={() =>
               editor.chain().focus().toggleHeading({ level: 1 }).run()
             }
@@ -85,6 +69,20 @@ export default function Editor() {
             />
             <div className="flex flex-col text-left">
               <span className="text-sm">Heading 1</span>
+              <span className="text-xs text-zinc-400">
+                Big section heading.
+              </span>
+            </div>
+          </button>
+
+          <button className="flex items-center gap-2 p-1 rounded min-w-[280px] hover:bg-zinc-600">
+            <img
+              src=""
+              alt="text"
+              className="w-12 border border-zinc-600 rounded"
+            />
+            <div className="flex flex-col text-left">
+              <span className="text-sm">Heading 2</span>
               <span className="text-xs text-zinc-400">
                 Big section heading.
               </span>
