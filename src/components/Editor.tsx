c656fe00cf9450992a195987eaea57e778ed41bd
julius caezar <julius.caezar25@gmail.com>
2023-07-31 18:33:34 -0400

    feat: Importando o component BubbleMenuComponent

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,4 +1,4 @@
-import { useEditor, EditorContent, BubbleMenu } from '@tiptap/react'
+import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight'
 import TaskItem from '@tiptap/extension-task-item'
@@ -6,19 +6,11 @@ import TaskList from '@tiptap/extension-task-list'
 
 import { lowlight } from 'lowlight'
 import js from 'highlight.js/lib/languages/javascript'
-import {
-  RxFontBold,
-  RxFontItalic,
-  RxStrikethrough,
-  RxCode,
-  RxChevronDown,
-  RxChatBubble
-} from 'react-icons/rx'
 
 import 'highlight.js/styles/atom-one-dark.css'
-import { BubbleButton } from './BubblleButton'
 import '../styles.css'
 import { FloatingMenuComponent } from './FloatingMenuComponent'
+import { BubbleMenuComponent } from './BubbleMenuComponent'
 
 lowlight.registerLanguage('js', js)
 
@@ -51,44 +43,7 @@ export default function Editor() {
       />
       {editor && <FloatingMenuComponent editor={editor} />}
 
-      {editor && (
-        <BubbleMenu
-          className="bg-zinc-700 shadow-xl border border-zinc-600 shadow-black/20 rounded-lg overflow-hidden flex divide-x divide-zinc-600"
-          editor={editor}
-        >
-          <BubbleButton>
-            Text
-            <RxChevronDown className="w-4 h-4" />
-          </BubbleButton>
-          <BubbleButton>
-            Comment
-            <RxChatBubble className="w-4 h-4" />
-          </BubbleButton>
-          <div className="flex items-center">
-            <BubbleButton
-              onClick={() => editor.chain().focus().toggleBold().run()}
-              data-active={editor.isActive('bold')}
-            >
-              <RxFontBold className="w-4 h-4" />
-            </BubbleButton>
-            <BubbleButton
-              onClick={() => editor.chain().focus().toggleItalic().run()}
-              data-active={editor.isActive('italic')}
-            >
-              <RxFontItalic className="w-4 h-4" />
-            </BubbleButton>
-            <BubbleButton
-              onClick={() => editor.chain().focus().toggleStrike().run()}
-              data-active={editor.isActive('strike')}
-            >
-              <RxStrikethrough className="w-4 h-4" />
-            </BubbleButton>
-            <BubbleButton>
-              <RxCode className="w-4 h-4" />
-            </BubbleButton>
-          </div>
-        </BubbleMenu>
-      )}
+      {editor && <BubbleMenuComponent editor={editor} />}
     </>
   )
 }
